1f26f1bc515f3f5308fcc720bfab843a088d0822
mayra lucia navarro <luciagirasoles@gmail.com>
2020-01-25 20:54:44 -0500

    generate user model

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+    has_secure_password
+    has_secure_token
+
+    def invalidate_token
+        update(token: nil)
+    end
+    
+    def self.valid_login?(email, password)
+    user = find_by(email: email)
+    user if user&.authenticate(password)
+    end
+
+end
