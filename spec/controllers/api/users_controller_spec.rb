dd7220dae3c62b68e10110e6b890e1fba14ca4af
mayra lucia navarro <luciagirasoles@gmail.com>
2020-01-30 07:01:44 -0500

    generate TDD tests generate method for bad request render on Api Controller

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -31,15 +31,40 @@ RSpec.describe Api::UsersController, type: :controller do
         it "returns http status created when user is logged and parans are missing" do
             allow(controller).to receive(:authenticate_token).and_return(@user_admin)
             post :create, params: {email: "something@wrong"}
-            p response.body
             expect(response).to have_http_status(:bad_request)
         end
 
         it "returns http bad request status when user email already exists" do  
             allow(controller).to receive(:authenticate_token).and_return(@user_admin)
             post :create, params: { user: {name: 'Juan', last_name: 'Perez', gender:"male", email: 'jperez@runa.co', password_digest: '123456', role: "employee"}}
-            expect(response.status).to have_http_status(:bad_request)
+            expect(response).to have_http_status(:bad_request)
         end
+    end
+
+    describe "GET index" do
+        it "returns status forbidden when there is not authorization included " do
+            get :index
+            expect(response).to have_http_status(:forbidden)
+        end
+
+        it "returns status unauthorized when there is not authorization included " do
+            allow(controller).to receive(:authenticate_token).and_return(@user_regular)
+            get :index
+            expect(response).to have_http_status(:unauthorized)
+        end
+
+        it "returns status ok when there is authorization included " do
+            allow(controller).to receive(:authenticate_token).and_return(@user_admin)
+            get :index
+            expect(response).to have_http_status(:ok)
+        end
+        it "returns the size of users created - 2 " do
+            allow(controller).to receive(:authenticate_token).and_return(@user_admin)
+            get :index
+            users= JSON.parse(response.body)
+            expect(users.size).to be(2)
+        end
+        
     end
 
 end
